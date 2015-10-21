4b23ee3f5a8b3a2961c92fa1b9827a216392b9ba
Chae Cramb <chaecramb@gmail.com>
2015-10-21 17:34:15 +0100

    add homework tasks

--- main.rb
+++ main.rb
@@ -32,23 +32,38 @@ post '/items' do
   details = params['details']
   sql = "INSERT INTO items (item, details) VALUES ('#{item}','#{details}')"
   run_sql(sql)
+  
+  sql = "SELECT id FROM items WHERE item = '#{item}' AND details = '#{details}'"
+  id = run_sql(sql).first['id'].to_i
 
-  redirect to('/items') # or can you get the id of the record created by the insert, and redirect to that?
+  redirect to("/items/#{id}") 
 end
 
-get '/items/:id' do # use this id to select one record from db, show that one record on screen
+get '/items/:id' do 
+  sql = "SELECT * FROM items WHERE id = #{params[:id].to_i}"
+  @item = run_sql(sql).first
   erb :show
 end
 
 get '/items/:id/edit' do
+  sql = "SELECT * FROM items WHERE id = #{params['id'].to_i}"
+  @item = run_sql(sql).first
   erb :edit
 end
 
 post '/items/:id' do
-  redirect to("/items/#{params[:id]}")
+  id = params['id'].to_i  
+  item = params['item']
+  details = params['details']
+  sql = "UPDATE items SET item='#{item}', details='#{details}' WHERE id='#{id}'"
+  run_sql(sql)
+  redirect to("/items/#{id}")
 end
 
 post '/items/:id/delete' do
+  id = params['id'].to_i 
+  sql = "DELETE FROM items WHERE id = #{id}"
+  run_sql(sql)
   redirect to('/')
 end
 
